ff03af1d23fb0925e461ef4cebd6a6d3aaf2dfb1
lppduy <lephamphuongduy@gmail.com>
2023-08-22 22:51:05 +0700

    v48/211

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // import ColorBox from 'components/ColorBox';
 // import Counter from 'components/Counter';
-// import AlbumFeature from 'features/Album';
+import AlbumFeature from 'features/Album';
 import TodoFeature from 'features/Todo';
 // import logo from './logo.svg';
 import React from 'react';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
-      <TodoFeature />
-      {/* <AlbumFeature /> */}
-      {/* <ColorBox />
-      <Counter /> */}
+      Home Page
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/albums" component={AlbumFeature} />
+      Footer
     </div>
   );
 }
