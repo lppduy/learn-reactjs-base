cf68615c93453e08dc29ac3c3b2f7e80a2e9fde8
lppduy <lephamphuongduy@gmail.com>
2023-08-22 23:00:39 +0700

    v49/211

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import AlbumFeature from 'features/Album';
 import TodoFeature from 'features/Todo';
 // import logo from './logo.svg';
 import React from 'react';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { NavLink, Link, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       Home Page
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/albums">Albums</Link>
+      </p>
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        {/* default: activeClassName="active" */}
+        <NavLink to="/albums">Albums</NavLink>
+      </p>
       <Route path="/todos" component={TodoFeature} />
       <Route path="/albums" component={AlbumFeature} />
       Footer
